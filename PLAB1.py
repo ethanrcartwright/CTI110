89e1e3770eaaf63e549783912a965cadcc32a900
ethanrcartwright <90797635+ethanrcartwright@users.noreply.github.com>
2021-09-16 11:01:13 -0400

    Add files via upload

---
+++ PLAB1.py
@@ -0,0 +1,12 @@
+# CTI 110
+# P1LAB1
+# Ethan
+# Cartwright
+# 09/16/2021
+
+# 7.12 from zybooks
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Math Bug fixed
+   
+print(userNumSquared)       # Output input fixed
