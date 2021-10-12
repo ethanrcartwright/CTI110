2dd59119f34f6531b10b86c73d7ca5d78fa05e3b
ethanrcartwright <90797635+ethanrcartwright@users.noreply.github.com>
2021-10-12 11:30:48 -0400

    Add files via upload
    Project for CTI 110. Had to debug the program. involved changing indents and correcting syntax.

---
+++ P3HW1_Debugging_CartwrightEthan.py
@@ -0,0 +1,42 @@
+# I was supposed to put a comment here
+# My Last Name
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60 
+
+    # TO DO: define the rest of the scores
+
+    
+    score = int(input('Enter grade: '))
+
+    if score >= A_score:
+      print('Your grade is: A')
+    elif score >= B_score:
+     print('Your grade is: B')
+    elif score >= C_score:
+      print('Your grade is: C')
+    elif score >= D_score:
+      print('Your grade is a: D')
+    else:
+      print('Your grade is a: F')
+    
+
+
+
+
+#programstart     
+main()
+
+
+
+
+
+
+
+# program start
